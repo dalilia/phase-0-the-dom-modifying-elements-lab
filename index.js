3c5a1fc5b31f415a0598ab9c16cec24d3d27026f
dalilia <dalila200875yahoo.fr>
2023-08-11 19:42:44 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main');
+mainElement.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML="Dalila is the champion"
+
